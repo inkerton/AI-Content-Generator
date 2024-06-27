ae2e54501cb820a563c9231bcc1d2e369408843d
Janvi <janvichoudhary116@gmail.com>
2024-06-27 22:40:16 +0530

    Project Setup completed, Clerk Integrated

---
+++ ai-content-generator/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { buttonVariants } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+    <div>
+      <h2>JANVI</h2>
+      <Button>my page</Button>
+    </div>
+  );
+}
